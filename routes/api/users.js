7ba2deb00fa044cce479286ac65796f43a39dc9f
Joanne Chen <joanne_r_chen@yahoo.com>
2020-11-25 14:21:18 +0000

    json web token created for security

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 // const { check, validationResult } = require('express-validator/check');
 
@@ -54,8 +56,20 @@ async (req, res) => {
 
         await user.save(); //save the user with encrypted password
         
-        //Return json webtoken
-        res.send('User registered!');
+        const payload = { //Returning json webtoken
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if(err) throw err;
+                res.json({ token });
+            });
     
     } catch(err) {
         console.error(err.message);
