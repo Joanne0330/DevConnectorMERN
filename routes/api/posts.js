086c9bf82ae72dd4ed209acb2fc4cf6e1658a474
Joanne Chen <joanne_r_chen@yahoo.com>
2020-11-30 14:33:54 +0000

    create react app and many extention installed

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -237,5 +237,6 @@ router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
         console.error(err.message);
         res.status(500).send('Server Error!');
     }
+    
 });
 module.exports = router;
