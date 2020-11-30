103fcefc1956950327b34447749ea7709e67d8ae
Joanne Chen <joanne_r_chen@yahoo.com>
2020-11-30 12:08:25 +0000

    can add and delete a comment from a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -164,4 +164,78 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 
 });
+
+//@route     POST api/posts/comment/:id  // need the id of the post to make comments
+//@desc      Comment on a post
+//@access    Private
+router.post(
+    '/comment/:id', 
+    [ 
+        auth, 
+        [
+            check('text', 'Text is required!')
+                .not()
+                .isEmpty()
+        ] 
+    ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        
+        try {  // now getting the name, id, and avatar from User model automatically
+            const user = await User.findById(req.user.id).select('-password'); // grab everything using auth, except password
+            const post = await Post.findById(req.params.id);  //find the post by id
+
+            const newComment = {
+                text: req.body.text, //setting the input text (req.body) to text
+                name: user.name,  // rest info come from User model
+                avatar: user.avatar,
+                user: req.user.id 
+            };
+
+            post.comments.unshift(newComment); // adding the comment into the post, newest one in the beginning
+            await post.save(); //save the post with added comment
+        
+            res.json(post.comments); //send the comment back
+
+        } catch(err) {
+            console.error(err.message);
+            res.status(500).send('Server Error!');
+        }
+});
+
+//@route     DELETE api/posts/comment/:id/:comment_id     //need post id and comment id
+//@desc      Delete a comment from a post
+//@access    Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);  //find the post by id
+
+        //Pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id); // find the comment id and compare with the req
+        
+        if(!comment) {   //check if this comment is found
+            return res.status(404).json({ msg: 'Comment does not exist!'});
+        }
+
+        if(comment.user.toString() !== req.user.id) {   //checking if the comment user id(ObjectID) is the same person requesting to delete
+            return res.status(401).json({ msg: 'User not authorsied to delete this comment!'});
+        }
+
+        // remove the index
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+        // map through the comments array, find the index number of the comment (in string format) that matches req.user's id
+        
+        post.comments.splice(removeIndex, 1); //splice the particular index
+        
+        await post.save();
+
+        res.json(post.comments);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error!');
+    }
+});
 module.exports = router;
