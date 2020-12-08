534827f855873c2146ae4bb0cfdb9c665ac653c6
Joanne Chen <joanne_r_chen@yahoo.com>
2020-12-08 11:30:12 +0000

    added profile experience and education on profile display

---
+++ client/src/components/profile/ProfileExperience.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+
+const ProfileExperience = ({ //experience is an array, but destructure the same way
+    experience: { company, title, location, current, to, from, description }
+}) => {
+    return (
+        <div>
+          <h3 className="text-dark">{company}</h3>  
+          <p>
+              <Moment format='YYYY/DD/MM'>{from}</Moment> - { !to ? 'Now' : 
+              <Moment format='YYYY/DD/MM'>{to}</Moment> }
+          </p>
+          <p>
+              <strong>Position: </strong>{title}
+          </p>
+          <p>
+              <strong>Description: </strong>{description}
+          </p>
+        </div>
+    )
+}
+
+ProfileExperience.propTypes = {
+    experience: PropTypes.array.isRequired,
+}
+
+export default ProfileExperience
