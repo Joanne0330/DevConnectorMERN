93a54e3c7c93c6b910a6f7b8dd35752fb20c3adf
Joanne Chen <joanne_r_chen@yahoo.com>
2020-12-08 16:00:40 +0000

    posts action and reducer created

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,12 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import profile from './profile';
-
+import post from './post';
 
 // this is the root reducer and bringing in other reducers
 export default combineReducers({
     alert, 
     auth,
-    profile
+    profile,
+    post
 });
--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -5,6 +5,7 @@ import axios from 'axios';  //just adding a global header
 const setAuthToken = token => {  //take in the token as a parameter and check
     if(token) {  //check if there's token in the localStorage, if there is:
         axios.defaults.headers.common['x-auth-token'] = token;    //set the global headers to token
+        localStorage.setItem('token', token);
     } else {
         delete axios.defaults.headers.common['x-auth-token'];
     }
