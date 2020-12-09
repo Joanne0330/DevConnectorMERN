9375ef7935045f054d7e43fe3aacb9eba48f4054
Joanne Chen <joanne_r_chen@yahoo.com>
2020-12-09 23:16:31 +0000

    delete post enabled

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -3,7 +3,8 @@ import { setAlert } from './alert';
 import {
     GET_POSTS,
     POST_ERROR, 
-    UPDATE_LIKES
+    UPDATE_LIKES, 
+    DELETE_POST
 } from './types';
 
 
@@ -45,7 +46,7 @@ export const addLike = id => async dispatch => { //passing in the post id
     }
 }
 
-// ** REmovelike
+// ** Remove like
 export const removeLike = id => async dispatch => { //passing in post id
     try {
         const res = await axios.put(`api/posts/unlike/${id}`);
@@ -54,6 +55,27 @@ export const removeLike = id => async dispatch => { //passing in post id
             type: UPDATE_LIKES,
             payload: { id, likes: res.data } //returning the post Id and the array of likes
         });
+    } catch (err) {
+        
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }  //the err msg and err status are from back end
+
+        });
+    }
+}
+
+// **Delete post
+export const deletePost= id => async dispatch => { //passing in post id
+    try {
+        await axios.delete(`api/posts/${id}`);
+
+        dispatch({
+            type: DELETE_POST,
+            payload: id //just give the post id to send to the reducer to filter out in the UI
+        });
+
+        dispatch(setAlert('Post removed!', 'success'));
     } catch (err) {
         
         dispatch({
